491f1b4a2bdebd8016c8b5f1f8c971c863bf2529
Hritik Kasaudhan <hritikblcm@gmail.com>
2024-01-28 20:16:15 +0530

    directory files of bootstrap goes in app.jsx to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Heading from "./Component/Heading";
 import Slogan from "./Component/Slogan";
 import TimePara from "./Component/TimePara";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import "./App.css"
 
 function App() {
